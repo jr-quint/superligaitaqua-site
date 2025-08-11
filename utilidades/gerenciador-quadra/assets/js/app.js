44e40842110f69d4456aa05ead91db0d72553e64
jr-quint <juniorquintilhano@hotmail.com>
2025-08-11 20:42:29 -0300

    move service-worker para pasta raiz da aplicação

--- utilidades/gerenciador-quadra/assets/js/app.js
+++ utilidades/gerenciador-quadra/assets/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/utilidades/gerenciador-quadra/assets/js/service-worker.js').then((registration) => {
+    navigator.serviceWorker.register('/utilidades/gerenciador-quadra/service-worker.js').then((registration) => {
       console.log('Service Worker registrado com sucesso:', registration);
     }).catch((error) => {
       console.log('Erro ao registrar o Service Worker:', error);
