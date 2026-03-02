affd63e8cec43878a1e2a9942ccf0e18b92766a6
jr-quint <juniorquintilhano@hotmail.com>
2026-03-02 20:52:23 -0300

    Remove codigo comentado

--- assets/js/formInscricao.js
+++ assets/js/formInscricao.js
@@ -1,22 +1,4 @@
 // Telefones
-// function aplicarMascaraTelefone(input) {
-//     let value = input.value.replace(/\D/g, '');
-
-//     if (value.length > 11) value = value.slice(0, 11);
-
-//     if (value.length > 10) {
-//         value = value.replace(/^(\d{2})(\d{5})(\d{4}).*/, '($1) $2-$3');
-//     } else if (value.length > 6) {
-//         value = value.replace(/^(\d{2})(\d{4})(\d+).*/, '($1) $2-$3');
-//     } else if (value.length > 2) {
-//         value = value.replace(/^(\d{2})(\d+).*/, '($1) $2');
-//     } else {
-//         value = value.replace(/^(\d*)/, '($1');
-//     }
-
-//     input.value = value;
-// }
-
 function aplicarMascaraTelefone(input) {
     let value = input.value.replace(/\D/g, '');
 
